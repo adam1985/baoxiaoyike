5a78f2a3c29aefcab64f759cb6c818323b0b4121
pangyuanxing <308106832@qq.com>
2014-06-25 18:05:06 +0800

    添加一套手机模板

---
+++ wp-content/themes/carrington-mobile/footer.php
@@ -0,0 +1,7 @@
+<div class="footer_content">
+	&#169; <?php print(date(Y)); ?> <?php bloginfo('name'); ?>&nbsp;&nbsp;<?php wp_loginout(); wp_register('  ', ''); ?><br/>
+	<p>Design by <a target="_blank" href="http://www.baoxiaoyike.cn">adam</a>&nbsp;&nbsp;</p>
+</div> 
+<?php wp_footer(); ?>
+</body>
+</html>
